51c5bc4bf004b395332fa1610484eb1566bdbb64
Shreyas <shreyasahir@users.noreply.github.com>
2016-05-22 15:50:51 -0700

    Update odbc.c

--- ext/odbc.c
+++ ext/odbc.c
@@ -152,40 +152,7 @@ static SQLRETURN tracesql(SQLHENV henv, SQLHDBC hdbc, SQLHSTMT hstmt,
 /* emulate rb_thread_call_without_gvl with rb_thread_blocking_region */
 //#define rb_thread_call_without_gvl(func, data1, ubf, data2) \
   //rb_thread_blocking_region((rb_blocking_function_t *)func, data1, ubf, data2)
-#ifndef HAVE_RB_THREAD_CALL_WITHOUT_GVL
-#ifdef HAVE_RB_THREAD_BLOCKING_REGION
 
-/* emulate rb_thread_call_without_gvl with rb_thread_blocking_region */
-#define rb_thread_call_without_gvl(func, data1, ubf, data2) \
-  rb_thread_blocking_region((rb_blocking_function_t *)func, data1, ubf, data2)
-
-#else /* ! HAVE_RB_THREAD_BLOCKING_REGION */
-/*
- * partial emulation of the 2.0 rb_thread_call_without_gvl under 1.8,
- * this is enough for dealing with blocking I/O functions in the
- * presence of threads.
- */
-
-#include <rubysig.h>
-#define RUBY_UBF_IO ((rb_unblock_function_t *)-1)
-typedef void rb_unblock_function_t(void *);
-static void *
-rb_thread_call_without_gvl(
-   void *(*func)(void *), void *data1,
-  RB_MYSQL_UNUSED rb_unblock_function_t *ubf,
-  RB_MYSQL_UNUSED void *data2)
-{
-  void *rv;
-
-  TRAP_BEG;
-  rv = func(data1);
-  TRAP_END;
-
-  return rv;
-}
-
-#endif /* ! HAVE_RB_THREAD_BLOCKING_REGION */
-#endif /* ! HAVE_RB_THREAD_CALL_WITHOUT_GVL */
   ///////////////////////////////////
   
 typedef struct _SQLExecDirect_Args {
