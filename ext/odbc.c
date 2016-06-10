5cee152295971b5ebdb68bae41bb1f5c6cebca49
Shreyas <shreyasahir@users.noreply.github.com>
2016-06-10 07:12:07 -0700

    Update odbc.c

--- ext/odbc.c
+++ ext/odbc.c
@@ -150,11 +150,11 @@ static SQLRETURN tracesql(SQLHENV henv, SQLHDBC hdbc, SQLHSTMT hstmt,
 ////////////////////////////////////////////////////////////////
 
 /* emulate rb_thread_call_without_gvl with rb_thread_blocking_region */
-//#define rb_thread_call_without_gvl(func, data1, ubf, data2) \
-  //rb_thread_blocking_region((rb_blocking_function_t *)func, data1, ubf, data2)
-
 #define rb_thread_call_without_gvl(func, data1, ubf, data2) \
-  rb_thread_call_without_gvl((rb_blocking_function_t *)func, data1, ubf, data2)
+  rb_thread_blocking_region((rb_blocking_function_t *)func, data1, ubf, data2)
+
+//#define rb_thread_call_without_gvl(func, data1, ubf, data2) \
+  //rb_thread_call_without_gvl((rb_blocking_function_t *)func, data1, ubf, data2)
   ///////////////////////////////////
   
 typedef struct _SQLExecDirect_Args {
